5b8a16b6fd8d0b8ffa9a72b55fabd751410ae42c
John Southworth <jx756k@att.com>
2018-07-17 10:34:21 -0700

    Wrapper around godoc redirector that can work in an fcgi context

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/fcgi"
+	"os"
+	"rsc.io/go-import-redirector/godoc"
+)
+
+var (
+	addr    string
+	vcs     string
+	fastcgi bool
+)
+
+func usage() {
+	fmt.Fprintf(os.Stderr, "usage: go-import-redirect <import> <repo>\n")
+	fmt.Fprintf(os.Stderr, "options:\n")
+	flag.PrintDefaults()
+	os.Exit(2)
+}
+
+func init() {
+	flag.StringVar(&addr, "addr", ":http", "serve http on `address`")
+	flag.StringVar(&vcs, "vcs", "git", "set version control `system`")
+	flag.BoolVar(&fastcgi, "fcgi", false, "use fcgi to serve requests")
+	flag.Usage = usage
+}
+
+func main() {
+	log.SetPrefix("go-import-redirect: ")
+	flag.Parse()
+	if flag.NArg() != 3 {
+		flag.Usage()
+	}
+	importPath := flag.Arg(0)
+	repoPath := flag.Arg(1)
+	servePath := flag.Arg(2)
+	mux := http.NewServeMux()
+	mux.Handle(servePath, godoc.Redirect(vcs, importPath, repoPath))
+	if fastcgi {
+		log.Fatal(fcgi.Serve(nil, mux))
+	} else {
+		log.Fatal(http.ListenAndServe(addr, mux))
+	}
+}
